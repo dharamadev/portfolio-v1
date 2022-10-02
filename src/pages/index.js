0b71eb7a0923655abb242560dc2c83a1ddd8d740
Dharmendra Awasthi <a13401651@MAC13401651-01.local>
2022-10-02 15:37:03 +0530

    Title added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Home from "./home";
 export default function Index() {
   return (
     <>
+       <Head>
+        <title>Portfolio | Dharmendra Awasthi</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Home />
     </>
   );
